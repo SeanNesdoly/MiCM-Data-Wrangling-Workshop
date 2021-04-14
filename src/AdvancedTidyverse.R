d75c3ce2ccfbaf0a839e38cf31cbd5eb6e088e96
Sean Nesdoly <srnesdoly@gmail.com>
2021-04-14 13:51:37 -0400

    Add in skeleton R files for tidyverse modules

---
+++ src/AdvancedTidyverse.R
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Advanced Tidyverse
+#
+# Sean Nesdoly
+# Data Wrangling Workshop
+# McGill initiative in Computational Medicine
+# 2021-04-15
+#-------------------------------------------------------------------------------
+
+# install.packages("tidyverse") # @TODO: uncomment & run if not yet installed
+library(tidyverse)
+
+# @TODO: Set working directory to where the 'MiCM-Data-Wrangling-Workshop'
+# GitHub repository is located ('-main' might be appended to the end).
+setwd("~/path/to/working/directory/MiCM-Data-Wrangling-Workshop")
